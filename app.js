4898ff8990ceb7893bd804d908f92c99fca4967c
Elga Ridlo <elgaridlo@Elgas-MacBook-Air.local>
2022-07-02 21:54:38 +0700

    change path

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(router)
 
 
 if(process.env.NODE_ENV ==='production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(process.cwd(), '/frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(process.cwd(), 'frontend', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => {
         res.send('API is running...')
